45575b16df4edb7ced6214c9cd0323efab6adbd7
tykimdream@korea.ac.kr <tykimdream@korea.ac.kr>
2022-12-18 23:48:33 +0900

    LandingPage

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className="App-logo">
+          <span className="App-text">SSAIBRARY</span>
+        </div>
       </header>
+      <div className="App-footer">
+        <div className="App-footer-text">Copy Right ©JJ Planet All Rights Reserved</div>
+      </div>
     </div>
   );
 }
