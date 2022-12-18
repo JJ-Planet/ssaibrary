7fb6f1c8eb921a0e4cb7b833862b92ae99f83a7f
unknown <tykimdream@gmail.com>
2022-12-19 03:25:38 +0900

    ReadMe, LandingPage, DB

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,20 +1,23 @@
-// import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import LandingPage from "./LandingPage";
+import Main from "./Main";
+import Product from "./Product";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <div className="App-logo">
-          <span className="App-text">SSAIBRARY</span>
-        </div>
-      </header>
-      <div className="App-footer">
-        <div className="App-footer-text">Copy Right ©JJ Planet All Rights Reserved</div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/Main" element={<Main />}></Route>
+          <Route path="/product/*" element={<Product />}></Route>
+          {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ fe/src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <>
+      <div className="Container">
+        <div className="InfoText">안녕하세요, 지환님</div>
+        <div className="WelcomeText">오늘도 행복한 하루 보내세요</div>
+        <div className="ProfileBox"></div>
+        <div className="ProfileImg"></div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
