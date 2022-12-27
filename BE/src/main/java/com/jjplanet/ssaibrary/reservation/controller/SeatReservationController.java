b9d6bec936fe016555a22b81cfbc2031047c5a86
misso88 <soyun9812@naver.com>
2022-12-27 23:50:05 +0900

    좌석예약 작업 및 예외처리 패키지 추가

--- BE/src/main/java/com/jjplanet/ssaibrary/reservation/controller/SeatReservationController.java
+++ BE/src/main/java/com/jjplanet/ssaibrary/reservation/controller/SeatReservationController.java
@@ -1,10 +1,46 @@
 package com.jjplanet.ssaibrary.reservation.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jjplanet.ssaibrary.reservation.domain.SeatReservation;
+import com.jjplanet.ssaibrary.reservation.dto.SeatReservationDTO;
+import com.jjplanet.ssaibrary.reservation.repository.SeatReservationRepository;
+import com.jjplanet.ssaibrary.reservation.service.SeatReservationService;
 
 @RestController
 @RequestMapping("/seat-reservation")
 public class SeatReservationController {
 
+	@Autowired
+	private SeatReservationService seatReservationService;
+
+	@PostMapping
+	public void reservationSeat(@RequestBody SeatReservationDTO seatReservationDTO) throws Exception {
+		seatReservationService.reservationSeat(seatReservationDTO);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteSeatReservation(@PathVariable Long id) throws Exception {
+		seatReservationService.deleteSeatReservation(id);
+	}
+
+	public List<SeatReservationDTO> findAllSeatReservation() throws Exception {
+
+		return null;
+	}
+
+
+	public SeatReservationDTO findSeatReservationById(Long id) throws Exception {
+	
+		return null;
+	}
 }
--- BE/src/main/java/com/jjplanet/ssaibrary/reservation/domain/SeatReservation.java
+++ BE/src/main/java/com/jjplanet/ssaibrary/reservation/domain/SeatReservation.java
@@ -6,18 +6,21 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import com.jjplanet.ssaibrary.member.domain.Member;
 import com.jjplanet.ssaibrary.seat.domain.Seat;
 
 @Entity
 @Getter
+@Setter
 @AllArgsConstructor(access = AccessLevel.PROTECTED) // 동일한 패키지 내 클래스에서만 객체 생성
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SeatReservation {
@@ -29,36 +32,36 @@ public class SeatReservation {
 
 	// 예약자아이디, 예약자닉네임
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id")
-	@JoinColumn(name = "member_nickname")
+	@JoinColumns({ @JoinColumn(name = "member_id", referencedColumnName = "id"),
+			@JoinColumn(name = "member_nickname", referencedColumnName = "nickname") })
 	private Member member;
 
 	// 좌석번호
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "seat_id")
 	private Seat seat;
-	
+
 	// 예약일시
 	private String reservationDate;
-	
+
 	// 예약시작일시
 	private String startDate;
-	
+
 	// 예약시간
 	private int time;
-	
+
 	// 입실시간
 	private String checkinDate;
-	
+
 	// 퇴실시간
 	private String checkoutDate;
-	
+
 	// 연장시간
 	private int addTime;
-	
+
 	// 연장횟수
 	private int addCount;
-	
+
 	// 상태(W:대기,A:사용중,C:사용완료,X:취소)
 	private char status;
 }
