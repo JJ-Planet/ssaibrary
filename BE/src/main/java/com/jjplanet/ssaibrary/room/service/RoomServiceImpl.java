8c0f2068f509c2e38dcf34a8f19c9b06e600c5f4
quid-0 <wodnd101@naver.com>
2022-12-31 21:02:49 +0900

    참고용

--- BE/src/main/java/com/jjplanet/ssaibrary/room/service/RoomServiceImpl.java
+++ BE/src/main/java/com/jjplanet/ssaibrary/room/service/RoomServiceImpl.java
@@ -3,6 +3,9 @@ package com.jjplanet.ssaibrary.room.service;
 import java.util.LinkedList;
 import java.util.List;
 
+import java.util.stream.Collectors;
+import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,45 +15,37 @@ import com.jjplanet.ssaibrary.room.dto.RoomDTO;
 import com.jjplanet.ssaibrary.room.repository.RoomRepository;
 
 @Service
+@RequiredArgsConstructor
 public class RoomServiceImpl implements RoomService {
 
-	@Autowired
-	private RoomRepository roomRepository;
+	private final RoomRepository roomRepository;
+
 
 	@Override
 	public List<RoomDTO> findAllRoom() throws Exception {
-		List<Room> roomList = roomRepository.findAll();
-		List<RoomDTO> roomDTOList = new LinkedList<>();
-		for (Room r : roomList) {
-			roomDTOList.add(
-					new RoomDTO(r.getId(), r.getFloor(), r.getTotalSeat(), r.getReserveSeat(), r.getIsAvailable()));
-		}
-		return roomDTOList;
+		return roomRepository.findAll().stream().map(Room::toDtoWithRoom).collect(Collectors.toList());
 	}
 
 	@Override
 	public RoomDTO findRoomById(Long id) throws Exception {
-		Room room = roomRepository.findById(id).orElseThrow(NotFoundException::new);
-		RoomDTO roomDTO = new RoomDTO(room.getId(), room.getFloor(), room.getTotalSeat(), room.getReserveSeat(),
-				room.getIsAvailable());
-		return roomDTO;
+		return roomRepository.findById(id).orElseThrow(NotFoundException::new).toDto();
 	}
 
 	@Override
 	public void insertRoom(RoomDTO roomDTO) throws Exception {
-		Room room = new Room(roomDTO.getId(), roomDTO.getFloor(), roomDTO.getTotalSeat(), roomDTO.getReserveSeat(), roomDTO.getIsAvailable());
+		Room room = Room.builder().roomDTO(roomDTO).build();
+		// or
+		Room room2 = new Room(roomDTO);
+
 		roomRepository.save(room);
 	}
 
 	@Override
+	@Transactional
 	public void updateRoom(RoomDTO roomDTO) throws Exception {
 		Room updateRoom = roomRepository.findById(roomDTO.getId()).orElseThrow(NotFoundException::new);
 
-		updateRoom.setFloor(roomDTO.getFloor());
-		updateRoom.setTotalSeat(roomDTO.getTotalSeat());
-		updateRoom.setReserveSeat(roomDTO.getReserveSeat());
-		updateRoom.setIsAvailable(roomDTO.getIsAvailable());
-		roomRepository.save(updateRoom);
+		updateRoom.updateRoom(roomDTO);
 	}
 
 	@Override
