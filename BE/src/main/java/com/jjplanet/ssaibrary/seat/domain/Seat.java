7d1b1ecbb67bcc8580b5f7134be9a6e4f3e8a38d
김소윤 <soyun9812@naver.com>
2023-01-04 21:57:55 +0900

    Seat Room develop

--- BE/src/main/java/com/jjplanet/ssaibrary/seat/domain/Seat.java
+++ BE/src/main/java/com/jjplanet/ssaibrary/seat/domain/Seat.java
@@ -6,30 +6,22 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import com.jjplanet.ssaibrary.room.domain.Room;
-import com.jjplanet.ssaibrary.room.service.RoomService;
 import com.jjplanet.ssaibrary.seat.domain.Seat;
 import com.jjplanet.ssaibrary.seat.dto.SeatDTO;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 동일한 패키지 내 클래스에서만 객체 생성
 @AllArgsConstructor
 public class Seat {
 
-	@Autowired
-	private RoomService roomService;
-	
 	// 좌석번호
 	@Id
 	private Long id;
@@ -44,7 +36,7 @@ public class Seat {
 
 	// 상태(W:대기중,R:예약중,A:사용중,X:이용불가능)
 	private char status;
-	
+
 	@Builder
 	public Seat(SeatDTO seatDTO, Room room) {
 		this.room = room;
